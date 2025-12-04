1d0629409be74f7d6580c74059279ecd97b26610
Collins Self <collins.self@maplarge.com>
2025-12-03 23:47:19 -0500

    quickfix: makes modal editable and calls save

--- frontend/components/DocumentationModal.tsx
+++ frontend/components/DocumentationModal.tsx
@@ -1,7 +1,9 @@
 import { COLORS } from "@/constants/colors";
 import { Patient } from "@/constants/types";
+import { apiFetch } from "@/lib/api";
 import { Ionicons } from "@expo/vector-icons";
-import { Modal, Text, View, Pressable, StyleSheet } from "react-native";
+import { useEffect, useState } from "react";
+import { Modal, Text, View, Pressable, StyleSheet, ActivityIndicator, TextInput } from "react-native";
 
 interface DocumentationModalProps {
     patient?: Patient | null;
@@ -10,7 +12,31 @@ interface DocumentationModalProps {
     setDocPatient: (p: Patient | null) => void;
 }
 
-export default function DocumentationModal({ patient, patientSummary, setDocPatient }: DocumentationModalProps) {
+export default function DocumentationModal({ patient, patientSummary, setDocPatient, showSave }: DocumentationModalProps) {
+    const [loading, setLoading] = useState<boolean>(false);
+    const [patSum, setPatSum] = useState<string | null>();
+
+    useEffect(() => setPatSum(patientSummary), [])
+
+    const handleSave = async () => {
+        setLoading(true);
+        try {
+            const url = `http://localhost:8000/patients/${patient?.Id}/save_summary?summary_text=${encodeURIComponent(patSum || "")}`;
+
+            const res = await apiFetch(url, {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+            });
+
+            console.log("Save summary result:", await res.json());
+        } catch (e) {
+            console.error("Failed to save note", e);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+
     return (
         <Modal
             visible={!!patient}
@@ -25,17 +51,34 @@ export default function DocumentationModal({ patient, patientSummary, setDocPati
                         <Text style={styles.modalHeaderText}>
                             {patient?.FIRST} {patient?.LAST}
                         </Text>
+                        <View style={styles.modalHeaderBtns}>
+                            {loading ? <ActivityIndicator size="small" color="#fff" /> : null}
 
-                        <Pressable onPress={() => setDocPatient(null)}>
-                            <Ionicons name="close" size={28} color="white" />
-                        </Pressable>
+                            {showSave ?
+                                <Pressable onPress={() => handleSave()}>
+                                    <Ionicons name="save" size={28} color="white" />
+                                </Pressable>
+                                : null}
+                            <Pressable onPress={() => setDocPatient(null)}>
+                                <Ionicons name="close" size={28} color="white" />
+                            </Pressable>
+                        </View>
                     </View>
 
                     {/* MAIN CONTENT */}
                     <View style={styles.modalContent}>
-                        <Text style={{ color: "#333" }}>
-                            {patientSummary}
-                        </Text>
+                        {showSave ? <TextInput
+                            style={styles.textInput}
+                            multiline
+                            value={patSum!}
+                            onChangeText={setPatSum}
+                            textAlignVertical="top"
+                            placeholderTextColor="#999"
+                        /> :
+                            <Text style={{ color: "#333" }}>
+                                {patientSummary}
+                            </Text>
+                        }
                     </View>
 
                 </View>
@@ -70,6 +113,12 @@ const styles = StyleSheet.create({
         justifyContent: "space-between",
         alignItems: "center",
     },
+    modalHeaderBtns: {
+        backgroundColor: COLORS.primary,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+    },
 
     modalHeaderText: {
         color: "white",
@@ -82,5 +131,16 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: "white",
         padding: 20,
+    },
+
+    textInput: {
+        height: "100%",
+        borderColor: "#ccc",
+        borderWidth: 1,
+        borderRadius: 8,
+        padding: 10,
+        fontSize: 14,
+        color: "#000",
+        backgroundColor: "#fafafa",
     },
 })
